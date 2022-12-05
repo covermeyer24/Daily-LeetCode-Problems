e372f7837e5cb06605699eb12032944c40139703
covermeyer24 <69399644+covermeyer24@users.noreply.github.com>
2022-12-04 22:06:07 -0600

    Update 2225-Find-Players-With-Zero-or-One-Losses.cc

--- 2022/2225-Find-Players-With-Zero-or-One-Losses.cc
+++ 2022/2225-Find-Players-With-Zero-or-One-Losses.cc
@@ -1,29 +1,37 @@
 vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         // set<int> losers;
         // set<int> dlosers;
+        
+        //map layout [team #, [losses, wins]]
         map<int, pair<int, int>> map;
         // set<int> winner;
         vector<vector<int>> res;
         vector<int> winners;
         vector<int> oneLoser;
         
+        //Goes through the matches array
         for (auto vec : matches) {
             int win = vec[0];
             int lose = vec[1];
             
             // winner.push(win);
+                
+            //Increases the losing teams loss counter, and increases the winning teams win counter
             map[lose].first++;
             map[win].second++;
         }
         
         for (auto i : map) {
+            
+            //If the team's losses is zero, and the team's wins are not zero, add it to the winners array
             if (i.second.first == 0 && i.second.second != 0) {
                 winners.push_back(i.first);
-            } else if (i.second.first == 1) {
+            } else if (i.second.first == 1) { //If the team only has one loss, add it to the losers array
                 oneLoser.push_back(i.first);
             }
         }
         
+        //Push the two arrays back into the return array
         res.push_back(winners);
         res.push_back(oneLoser);
         

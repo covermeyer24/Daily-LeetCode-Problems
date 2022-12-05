e330fc7b840d4d8bf5c77ab57916889ad1e6df3b
covermeyer24 <69399644+covermeyer24@users.noreply.github.com>
2022-12-04 21:58:30 -0600

    Update 1704-Determine-if-String-Halves-Are-Alike.cc

--- 2022/1704-Determine-if-String-Halves-Are-Alike.cc
+++ 2022/1704-Determine-if-String-Halves-Are-Alike.cc
@@ -1,17 +1,21 @@
 bool halvesAreAlike(string s) {
         int count = 0;
-        
+    
+        //Checks how many vowels are in the left half of the string
         for (int i = 0; i < s.size() / 2; i++) {
             if (isVowel(s[i])) count++;
         }
         
+        //Checks how many vowels are in the right half of the string
         for (int i = s.size() / 2; i < s.size(); i++) {
             if (isVowel(s[i])) count--;
         }
         
+        //If the count is zero then the string halves have the same amount of vowels
         return !count;
     }
     
+    //Takes in character to check if it is a vowel
     bool isVowel(char& c) {
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || 
            c == 'E' || c == 'I' || c == 'O' || c == 'U') return true;
